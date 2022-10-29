dd1de293188220bc80f71ed78c390200eb6078fd
Quinn <qnceup@gmail.com>
2022-10-29 22:10:11 +0200

    Added new tests in dataAnalysis.R

--- results/dataAnalysis.R
+++ results/dataAnalysis.R
@@ -1,64 +1,87 @@
-#install.packages('tidyverse') 
-#install.packages('car') 
-#install.packages('bestNormalize') 
+#install.packages('stringi')
+#install.packages('tidyverse')
+#install.packages('car')
+#install.packages('bestNormalize')
 #install.packages('effsize')
+#install.packages('stringr')
+#install.packages('bestNormalize')
+#install.packages('ggstatsplot')
 
+library(effsize)
+library(ggstatsplot)
+library(bestNormalize)
+library(stringr)
 library(tidyverse)
 library(ggplot2)
-#library(car) 
-library(bestNormalize) 
-library(effsize) 
+library(bestNormalize)
+library(effsize)
+library(dplyr)
 
-df <- read_csv("/Users/quinn/Desktop/android-runner/data/complete_results_20221022.csv")
+df <- read_csv("/Users/quinn/Desktop/android-runner/results/complete_results_20221029.csv")
 head(df)
 
-levels(df$Name)
-levels(df$AndroidApp)
 
+# ======= Raw data analysis =======
 summary(df)
 
-df %>% group_by(Name) %>% 
-  summarize(mean_cons = mean(EnergyConsumption), 
-  sd_cons = sd(EnergyConsumption))
+native_apps <- df[df$AndroidApp == 1,]
+web_apps <- df[df$AndroidApp == 0,]
+cat('\n\n')
+print(quantile(native_apps$TotalEnergyConsumption))
+cat('\n\n')
+print(quantile(web_apps$TotalEnergyConsumption))
 
-#boxplot(EnergyConsumption~AndroidApp, df)
+outliers <- boxplot(TotalEnergyConsumption~AndroidApp, df, plot=FALSE)$out
+df_no_outliers<-df[-which(df$TotalEnergyConsumption %in% outliers),]
 
-# Outlier removal
-outliers <- boxplot(EnergyConsumption~AndroidApp, df, plot=FALSE)$out
-df<-df[-which(df$EnergyConsumption %in% outliers),]
+plot(ggstatsplot::ggbetweenstats(
+  data  = df,
+  x     = AndroidApp,
+  y     = TotalEnergyConsumption,
+  pairwise.comparisons = FALSE,
+))
 
-boxplot(EnergyConsumption~AndroidApp, df)
 
-check_normality <- function(data) { 
-  par(mfrow=c(1,2)) 
-  plot(density(data$EnergyConsumption)) 
-  #car::qqPlot(data) 
 
-  #qqplot(data1$Name,data1$EnergyConsumption, xlab =  "x",  ylab = "", main = "")
-  qqnorm(data$EnergyConsumption)
-  qqline(data$EnergyConsumption)
+# ======= Check for normality =======
+check_normality <- function(data) {
+  par(mfrow=c(1,2))
+  plot(density(data$TotalEnergyConsumption))
+  qqnorm(data$TotalEnergyConsumption)
+  qqline(data$TotalEnergyConsumption)
 
-  print(shapiro.test(data$EnergyConsumption)) 
-  par(mfrow=c(1,1)) 
-} 
+  cat('\n\n')
+  print(shapiro.test(data$TotalEnergyConsumption))
+  par(mfrow=c(1,1))
+}
 
-# Mutate data  
-df = df %>% 
-  mutate( 
-    cons_log = log(EnergyConsumption), 
-    cons_sqrt = sqrt(EnergyConsumption), 
-    cons_reciprocal = 1/EnergyConsumption 
-  ) 
+check_normality(native_apps)
+check_normality(web_apps)
 
-check_normality(df[df$AndroidApp == 1,]) 
-check_normality(df[df$AndroidApp == 0,]) 
 
-native_app <- df %>%
-  filter(AndroidApp == "1") %>%
-  pull(EnergyConsumption)
-web_app <- df %>%
-  filter(AndroidApp == "0") %>%
-  pull(EnergyConsumption)
 
-res <- t.test(native_app, web_app)
+# ======= Normalize the data =======
+cols_native <- as.numeric(unlist(c(native_apps %>% select(TotalEnergyConsumption))))
+order_object_native <- bestNormalize(cols_native)
+p_native <- predict(order_object_native)
+x2 <- predict(orderNorm_obj, newdata = p_native, inverse = TRUE)
+
+cols_web <- as.numeric(unlist(c(web_apps %>% select(TotalEnergyConsumption))))
+order_object_web <- bestNormalize(cols_web)
+p_web <- predict(order_object_web)
+x3 <- predict(order_object_web, newdata = p_web, inverse = TRUE)
+
+
+
+# ======= Hypothesis testing =======
+res <- t.test(x2, x3, alternative = "two.sided")
+cat('\n\n')
 print(res)
+
+
+
+# ======= Effect Size Estimation =======
+effect_size <- cohen.d(native_apps$TotalEnergyConsumption, web_apps$TotalEnergyConsumption)
+cat('\n\n')
+print(effect_size)
+
